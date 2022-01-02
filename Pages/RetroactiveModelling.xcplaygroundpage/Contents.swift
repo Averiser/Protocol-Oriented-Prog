3774125c497ec5ef78771b483277307779f6b48d
Averiser <averiser@gmail.com>
2022-01-02 12:07:18 +0300

    fifth commit - Challenge - Retroactive Modeling

--- Pages/RetroactiveModelling.xcplaygroundpage/Contents.swift
+++ Pages/RetroactiveModelling.xcplaygroundpage/Contents.swift
@@ -14,11 +14,15 @@ let rect = CGRect(origin: .zero, size: CGSize(width: 300, height: 200))
 protocol Geometry {
   var origin: CGPoint { get set }
   func area() -> CGFloat
+  func perimeter() -> CGFloat
 }
 
 extension Circle: Geometry {
   func area() -> CGFloat {
     return .pi * radius * radius
+  }
+  func perimeter() -> CGFloat {
+    return 2 * .pi * radius
   }
 }
 
@@ -26,9 +30,14 @@ extension CGRect: Geometry {
   func area() -> CGFloat {
     return size.width * size.height
   }
+  func perimeter() -> CGFloat {
+    return 2 * (size.width + size.height)
+  }
 }
 
 let shapes: [Geometry] = [circle, rect]
 let area = shapes.reduce(0) { $0 + $1.area() }
+let perimeter = shapes.reduce(0) { $0 + $1.perimeter() }
 
-print(area)
+//print(area)
+print(perimeter)
